9d90c7852c36fed99a6081ad73198e573732fef8
donny <donny.rolanda@gmail.com>
2022-06-17 09:52:38 +0800

    Improvement
    - [Position] : Fix wrong set position

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ var shelljs = require("shelljs");
 const writeYamlFile = require('write-yaml-file')
 require('dotenv').config();
 
-axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${process.env.LAT}&lon=${process.env.LAT}&appid=${process.env.API_KEY}`).then(function (data) {
+axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${process.env.LAT}&lon=${process.env.LONG}&appid=${process.env.API_KEY}`).then(function (data) {
   writeYamlFile('weather.yaml', data.data).then(() => {
     console.log('done')
   })
-  axios.post("http://192.168.50.4:3000/xhr/webhook/execute", {
+  axios.post(`${process.env.HOST_WEBHOOK}`, {
     data: JSON.stringify({ 
       "subject": `${process.env.JOB_ID}`, 
       "message": `${process.env.LINK}` })
